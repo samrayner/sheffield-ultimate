6086a157e84f3b20fa3caf56cee4fbe7244387ae
Sam Rayner <sam@terracoding.com>
2014-05-25 16:23:23 +0100

    Add subscription form to contact page

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,18 +1,37 @@
 class EmailsController < ApplicationController
   def new
-    @email = Email.new
+    @contact_email = Email.new
+    @subscription_email = Email.new
     render cms_page: "/contact"
   end
 
   def create
-    @email = Email.new(params[:email])
+    @contact_email = Email.new(params[:email])
+    @subscription_email = Email.new
 
-    if @email.valid?
-      if @email.submit
-        flash[:success] = "Thank you for your message. We'll respond as soon as we can."
-        return redirect_to(new_email_path)
+    success = "Thank you for your message. We'll respond as soon as we can."
+    failure = "Sorry, your message could not be delivered."
+    send_email(@contact_email, success, failure)
+  end
+
+  def subscribe
+    @contact_email = Email.new
+    @subscription_email = Email.new(params[:subscription_email])
+
+    success = "Thank you for subscribing. You'll soon receive updates from the club via email."
+    failure = "Sorry, there was a problem subscribing."
+    send_email(@subscription_email, success, failure)
+  end
+
+  private
+
+  def send_email(email, success, failure)
+    if email.valid?
+      if email.submit
+        flash[:success] = success
+        return redirect_to(contact_path)
       else
-        flash[:error] = "Sorry, your message could not be delivered."
+        flash[:error] = failure
       end
     end
 
--- spec/features/emails_spec.rb
+++ spec/features/emails_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe "Emails" do
   before :all do
@@ -9,55 +9,113 @@ describe "Emails" do
     delete_all_cms_data
   end
 
-  it "delivers a valid message" do
-    visit new_email_path
-    fill_in 'Name', with: 'Sam Rayner'
-    fill_in 'Email', with: 'sam@samrayner.com'
-    fill_in 'Message', with: 'What a great website.'
-    click_button 'Send'
-    page.body.should have_content('Thank you')
+  before do
+    visit contact_path
   end
 
-  it "does not deliver a message with a missing name" do
-    visit new_email_path
-    fill_in 'Email', with: 'sam@example.com'
-    fill_in 'Message', with: 'What a great website.'
-    click_button 'Send'
-    page.body.should have_content("can't be blank")
+  context "sending an email" do
+    it "delivers a valid message" do
+      within "#new_email" do
+        fill_in "Name", with: "Sam Rayner"
+        fill_in "Email", with: "sam@samrayner.com"
+        fill_in "Message", with: "What a great website."
+        click_button "Send"
+      end
+      page.body.should have_content("Thank you")
+    end
+
+    it "does not deliver a message with a missing name" do
+      within "#new_email" do
+        fill_in "Email", with: "sam@example.com"
+        fill_in "Message", with: "What a great website."
+        click_button "Send"
+      end
+      page.body.should have_content("can't be blank")
+    end
+
+    it "does not deliver a message with an invalid email" do
+      within "#new_email" do
+        fill_in "Name", with: "Sam Rayner"
+        fill_in "Email", with: "not_an_email"
+        fill_in "Message", with: "What a great website."
+        click_button "Send"
+      end
+      page.body.should have_content("is invalid")
+    end
+
+    it "does not deliver a message with a blank email" do
+      within "#new_email" do
+        fill_in "Name", with: "Sam Rayner"
+        fill_in "Message", with: "What a great website."
+        click_button "Send"
+      end
+      page.body.should have_content("can't be blank")
+    end
+
+    it "does not deliver a message with a blank message" do
+      within "#new_email" do
+        fill_in "Name", with: "Sam Rayner"
+        fill_in "Email", with: "sam@example.com"
+        click_button "Send"
+      end
+      page.body.should have_content("can't be blank")
+    end
+
+    it "does not deliver spam" do
+      within "#new_email" do
+        fill_in "Name", with: "Sam Rayner"
+        fill_in "Email", with: "spammer@spammyjunk.com"
+        fill_in "Message", with: "All the junk you'll never need."
+        fill_in "First name", with: "Want to buy some gold?"
+        click_button "Send"
+      end
+      page.body.should have_content("must be blank")
+    end
   end
 
-  it "does not deliver a message with an invalid email" do
-    visit new_email_path
-    fill_in 'Name', with: 'Sam Rayner'
-    fill_in 'Email', with: 'not_an_email'
-    fill_in 'Message', with: 'What a great website.'
-    click_button 'Send'
-    page.body.should have_content("is invalid")
-  end
+  context "subscribing to receive emails" do
+    it "delivers a valid message" do
+      within "#new_subscription_email" do
+        fill_in "Name", with: "Sam Rayner"
+        fill_in "Email", with: "sam@samrayner.com"
+        click_button "Subscribe"
+      end
+      page.body.should have_content("Thank you")
+    end
 
-  it "does not deliver a message with a blank email" do
-    visit new_email_path
-    fill_in 'Name', with: 'Sam Rayner'
-    fill_in 'Message', with: 'What a great website.'
-    click_button 'Send'
-    page.body.should have_content("can't be blank")
-  end
+    it "does not deliver a message with a missing name" do
+      within "#new_subscription_email" do
+        fill_in "Email", with: "sam@example.com"
+        click_button "Subscribe"
+      end
+      page.body.should have_content("can't be blank")
+    end
 
-  it "does not deliver a message with a blank message" do
-    visit new_email_path
-    fill_in 'Name', with: 'Sam Rayner'
-    fill_in 'Email', with: 'sam@example.com'
-    click_button 'Send'
-    page.body.should have_content("can't be blank")
-  end
+    it "does not deliver a message with an invalid email" do
+      within "#new_subscription_email" do
+        fill_in "Name", with: "Sam Rayner"
+        fill_in "Email", with: "not_an_email"
+        click_button "Subscribe"
+      end
+      page.body.should have_content("is invalid")
+    end
 
-  it "does not deliver spam" do
-    visit new_email_path
-    fill_in 'Name', with: 'Sam Rayner'
-    fill_in 'Email', with: 'spammer@spammyjunk.com'
-    fill_in 'Message', with: "All the junk you'll never need."
-    fill_in 'First name', with: 'Want to buy some gold?'
-    click_button 'Send'
-    page.body.should have_content('must be blank')
+    it "does not deliver a message with a blank email" do
+      within "#new_subscription_email" do
+        fill_in "Name", with: "Sam Rayner"
+        click_button "Subscribe"
+      end
+      page.body.should have_content("can't be blank")
+    end
+
+    it "does not deliver spam" do
+      within "#new_subscription_email" do
+        fill_in "Name", with: "Sam Rayner"
+        fill_in "Email", with: "spammer@spammyjunk.com"
+        fill_in "First name", with: "Want to buy some gold?"
+        click_button "Subscribe"
+      end
+      page.body.should have_content("must be blank")
+    end
   end
 end
