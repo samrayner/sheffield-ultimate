598319e44735b472f4fca54d98c4d13c79db1f45
Sam Rayner <sam@terracoding.com>
2016-07-27 09:47:35 +0100

    Swap SMTP to SendGrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,11 +31,11 @@ SheffieldUltimate::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
     domain:               'localhost:3000',
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_PASSWORD"],
+    user_name:            ENV["SMTP_USERNAME"],
+    password:             ENV["SMTP_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true
   }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,11 @@ SheffieldUltimate::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.net',
     port:                 587,
-    domain:               'sheffieldultimate.co.uk',
-    user_name:            ENV["GMAIL_USERNAME"],
-    password:             ENV["GMAIL_PASSWORD"],
+    domain:               'www.sheffieldultimate.co.uk',
+    user_name:            ENV["SMTP_USERNAME"],
+    password:             ENV["SMTP_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true
   }
